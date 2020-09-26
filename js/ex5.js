318b662b684da16264a8931405d2c5d3cf909340
Bryan <bryanwong@student.csulb.edu>
2020-09-26 15:03:18 -0700

    snapshot 2

--- js/ex5.js
+++ js/ex5.js
@@ -1,11 +1,11 @@
-function vowel_count(str1)
+function count(str)
 {
-  let vowel_list = 'aeiou';
+  let list = 'aeiou';
   let vcount = 0;
   
-  for(let x = 0; x < str1.length ; x++)
+  for(let x = 0; x < str.length ; x++)
   {
-    if (vowel_list.indexOf(str1[x]) !== -1)
+    if (list.indexOf(str[x]) !== -1)
     {
       vcount += 1;
     }
@@ -14,4 +14,4 @@ function vowel_count(str1)
   return vcount;
 }
 
-console.log(vowel_count(prompt("Enter a word")));
+console.log(count(prompt("Enter a word")));
--- js/ex6.js
+++ js/ex6.js
@@ -1,17 +1,17 @@
-var num = Math.floor(Math.random() * 10) + 1;
+var correct = Math.floor(Math.random() * 10) + 1;
 var guess = prompt("guess");
 
 i = 1
 while (true){
-  if (num == guess){
+  if (correct == guess){
     console.log(`Correct, It took you ${i} attempts to guess the correct number`)
     break;
   }
-  else if (num < guess){
+  else if (correct < guess){
     guess = prompt("Too high, guess again");
     i++;
   }
-  else if (num > guess){
+  else if (correct > guess){
     guess = prompt("Too low, guess again");
     i++;
   }
